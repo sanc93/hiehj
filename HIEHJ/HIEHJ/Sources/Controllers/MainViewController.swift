1848fa471ab34993e6a8c12503deaa4905a930c2
sanc93 <sanc93@me.com>
2023-08-25 16:33:21 +0900

    프로젝트 생성

---
+++ HIEHJ/HIEHJ/Sources/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  HIEHJ
+//
+//  Created by Sanghun K. on 2023/08/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
